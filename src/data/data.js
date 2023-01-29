ce3fce666463c97999e49c87118666720ae031a2
Stuart <stuart.francis@hotmail.co.uk>
2023-01-29 21:40:57 +0000

    added more resources

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,15 @@
 export const data = {
+    categoryName: {
+        title: 'Most Used',
+        path: '/most-used',
+        resources: [
+            {
+                resourceName: "White Rose Maths",
+                description: "...",
+                url: "https://whiterosemaths.com/login"
+            }
+        ]
+    },
     starters: {
         title: 'Starters',
         path: '/starters',
@@ -11,14 +22,25 @@ export const data = {
             {
                 resourceName: "Corbett Maths 5-a-day",
                 description: "Daily 5 question starter on random topics at 5 different levels",
-                url: "https://corbettmaths.com/contents/"
+                url: "https://corbettmaths.com/5-a-day/gcse/"
+            },
+            {
+                resourceName: "Maths Box",
+                description: "...",
+                url: "https://www.mathsbox.org.uk/index1.php"
+            },
+            {
+                resourceName: "Diagnostic Questions",
+                description: "...",
+                url: "https://diagnosticquestions.com/Account/Login"
             }
+
         ]
     },
 
     practice: {
-        title: 'Practice',
-        path: '/practice',
+        title: 'Main',
+        path: '/main',
         resources: [
             {
                 resourceName: "Maths Genie Questions",
@@ -34,6 +56,48 @@ export const data = {
                 resourceName: "Dr Frost",
                 description: "PowerPoints and Worksheets, especially good for higher ability.",
                 url: "https://www.drfrostmaths.com/downloadables.php?"
+            },
+            {
+                resourceName: "Resourceaholic",
+                description: "Collated resources covering all of maths!",
+                url: "https://www.resourceaholic.com/"
+            },
+            {
+                resourceName: "Increasingly Difficult Questions",
+                description: "Worksheets with questions ranging from easy to hard on a single topic",
+                url: "https://taylorda01.weebly.com/increasingly-difficult-questions.html"
+            },
+            {
+                resourceName: "Go Teach Maths",
+                description: "...",
+                url: "https://www.goteachmaths.co.uk/"
+            }
+        ]
+    },
+
+    categoryName: {
+        title: 'Challenge / Extensions',
+        path: '/challenge',
+        resources: [
+            {
+                resourceName: "UKMT Random Question Generator",
+                description: "...",
+                url: "https://www.interactive-maths.com/ukmt-random-question-generator.html"
+            },
+            {
+                resourceName: "Mr Barton Venns",
+                description: "...",
+                url: "https://www.mrbartonmaths.com/teachers/rich-tasks/venn-diagrams.html"
+            },
+            {
+                resourceName: "Median",
+                description: "...",
+                url: "..."
+            },
+            {
+                resourceName: "N-rich",
+                description: "...",
+                url: "..."
             }
         ]
     },
@@ -46,6 +110,16 @@ export const data = {
                 resourceName: "Exam Q Website",
                 description: "Search and filter Edexcel Exam Questions by topic and tier",
                 url: "https://www.examq.co.uk/"
+            },
+            {
+                resourceName: "Exam Wizard",
+                description: "...",
+                url: "https://www.examwizard.co.uk/"
+            },
+            {
+                resourceName: "Maths Emporium",
+                description: "...",
+                url: "https://mathsemporium.com/login/"
             }
         ]
     },
